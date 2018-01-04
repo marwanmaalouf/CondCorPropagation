4a34c7a5b17c070d079d0db73f48f1d537150993
marwanmaalouf <marwanmaalouf1996@gmail.com>
2018-01-04 14:53:35 +0200

    Fixed bat files

--- check.bat
+++
@@ -1,4 +0,0 @@
-cls
-echo "Printing class bytecode"
-javac -cp "./src/libs/*";"./" Visualizer\Visualize.java 
-java  -cp "./src/libs/*";"./Visualizer/" Visualize C:\Users\User\Desktop\ISSTA\instrumented\classes\Test1.class
--- printBytecode.bat
+++ printBytecode.bat
@@ -1,4 +1,5 @@
+REM check [path to class file]
 cls
 echo "Printing class bytecode"
 javac -cp "./src/libs/*";"./" Visualizer\Visualize.java 
-java  -cp "./src/libs/*";"./Visualizer/" Visualize .\SourceCodeToTest\Test.class 
+java  -cp "./src/libs/*";"./Visualizer/" Visualize %1
